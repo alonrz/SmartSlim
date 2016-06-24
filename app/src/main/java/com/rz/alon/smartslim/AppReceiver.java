f05bd6ffb339312464d318052901dc551203b21d
Alon Reich-Zilberman <alonrz@yahoo-inc.com>
2016-06-24 15:03:54 -0700

    timer works and wake up after 5 second, but starts fresh

---
+++ app/src/main/java/com/rz/alon/smartslim/AppReceiver.java
@@ -0,0 +1,41 @@
+package com.rz.alon.smartslim;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by alonrz on 6/24/16.
+ */
+public class AppReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.d("ALON", "AppReceiver");
+        Intent newIntent = new Intent(context, TimerActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(newIntent);
+    }
+
+    public void setAlarm(Context context) {
+        Log.d("ALON", "setAlarm");
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AppReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        //alarmManager.setInexactRepeating(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis() + mMillisLeft, 0, pendingIntent);
+        alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), 5000, pendingIntent);
+    }
+
+    public void cancelAlarm(Context context) {
+//        Intent intent = new Intent(context, Alarm.class);
+//        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+//        alarmManager.cancel(sender);
+    }
+}
--- app/src/main/java/com/rz/alon/smartslim/TimerActivity.java
+++ app/src/main/java/com/rz/alon/smartslim/TimerActivity.java
@@ -1,11 +1,8 @@
 package com.rz.alon.smartslim;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -35,11 +32,13 @@ public class TimerActivity extends AppCompatActivity {
     CountDownTimer mCounter;
     long mMillisLeft;
     boolean isTimerRunning = false;
+    AppReceiver mAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+        mAlarm = new AppReceiver();
         btnAdd3 = (Button) findViewById(R.id.btnAdd3);
         btnAdd3Half = (Button) findViewById(R.id.btnAdd3Half);
         btnAdd2Half = (Button) findViewById(R.id.btnAdd2Half);
@@ -53,11 +52,16 @@ public class TimerActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Intent intent = new Intent(this, AlarmReciever)
-        PendingIntent pendingIntent = PendingIntent.get
-        AlarmManager alarmManager = (AlarmManager) this.getSystemService(ALARM_SERVICE);
-        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, 10, );
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+       mAlarm.setAlarm(this);
+    }
+
 
     private void addClickActions() {
         btnAdd3.setOnClickListener(new View.OnClickListener() {
